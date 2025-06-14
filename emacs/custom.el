10ac73929a56496809afde2dc07ba79da1a104ec
levindue <levin@duesterhus.net>
2025-06-14 20:18:09 +0200

    [Emacs] restructure config, add magit

--- emacs/custom.el
+++ emacs/custom.el
@@ -6,12 +6,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
+   '("ae20535e46a88faea5d65775ca5510c7385cbf334dfa7dde93c0cd22ed663ba0"
+     "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      default))
  '(haskell-interactive-popup-errors nil)
  '(package-selected-packages
-   '(consult corfu gruber-darker-theme haskell-mode odin-mode orderless
-             rust-mode treesit-auto typst-ts-mode vertico))
+   '(consult corfu ef-themes gruber-darker-theme haskell-mode magit
+             odin-mode orderless rust-mode typst-ts-mode
+             vertico))
  '(package-vc-selected-packages
    '((odin-mode :url "https://git.sr.ht/~mgmarlow/odin-mode")
      (typst-ts-mode :url
--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,5 @@
+;; Better Defaults
 
-(setq-default frame-title-format '("Emacs"))
 (blink-cursor-mode -1)
 (scroll-bar-mode -1)
 (fringe-mode -1)
@@ -8,24 +8,15 @@
 
 (setq ring-bell-function 'ignore)
 
-(setq display-line-numbers-type 'relative) 
-(global-display-line-numbers-mode)
-
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
 (setq scroll-step 1)
 (setq scroll-conservatively 101)
 (setq pixel-scroll-precision-mode t)
 (setq pixel-scroll-precision-use-momentum nil)
 
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq c-default-style "bsd") 
+(setq-default tab-width 4)
+(setq c-default-style "bsd")
 (setq c-basic-offset 4)
-
-(electric-pair-mode 1)
-(column-number-mode 1)
 
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
@@ -34,14 +25,30 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(global-unset-key (kbd "C-z"))
-(global-unset-key (kbd "C-x C-z"))
+;; custom file
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load-file custom-file))
 
+;; Options
+
+(setq-default frame-title-format '("Emacs"))
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+
+(electric-pair-mode 1)
+(column-number-mode 1)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
 (set-frame-font "JetBrainsMono 14" nil t)
+
+;; Keybinds
+
+(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x C-z"))
 
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "C-x b") 'consult-buffer)
@@ -49,24 +56,32 @@
 (global-set-key (kbd "M-M") 'compile)
 (global-set-key (kbd "M-m") 'recompile)
 
+;; Packages
+
 (require 'package)
 (package-initialize)
 
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
+(with-eval-after-load 'package
+  (setq package-archives
+        '(("gnu" . "http://elpa.gnu.org/packages/")
+          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+          ("melpa" . "https://melpa.org/packages/"))))
 
-(use-package gruber-darker-theme
-  :ensure t)
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-(use-package orderless
-  :ensure t
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles basic partial-completion)))))
+;; Themes
+
+(use-package gruber-darker-theme)
+(use-package ef-themes)
+
+;; Git
+
+(use-package magit)
+
+;; Minibuffer
 
 (use-package vertico
-  :ensure t
   :init
   (vertico-mode 1))
 
@@ -85,40 +100,35 @@
               ("M-DEL" . vertico-directory-delete-word))
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
-(use-package consult
-  :ensure t)
+(use-package consult)
+
+;; completion
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package corfu
-  :ensure t
   :init
   (global-corfu-mode))
 
-(setq treesit-language-source-alist
-      '((ocaml "https://github.com/tree-sitter/tree-sitter-ocaml" "master" "grammars/ocaml/src/")
-        (ocaml-interface "https://github.com/tree-sitter/tree-sitter-ocaml" "master" "grammars/interface/src/")))
-
-(use-package treesit-auto
-  :ensure t
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
+;; languages
 
 (use-package typst-ts-mode
-  :vc (:url "https://codeberg.org/meow_king/typst-ts-mode.git" 
+  :vc (:url "https://codeberg.org/meow_king/typst-ts-mode.git"
             :rev :newest))
 
 (use-package odin-mode
   :vc (:url "https://git.sr.ht/~mgmarlow/odin-mode"
             :rev :newest))
 
-(use-package haskell-mode
-  :ensure t)
+(use-package haskell-mode)
 
 (let ((my-ghcup-path (expand-file-name "~/.ghcup/bin")))
   (setenv "PATH" (concat my-ghcup-path ":" (getenv "PATH")))
   (add-to-list 'exec-path my-ghcup-path))
 
 (use-package rust-mode
-  :ensure t)
+  :custom
+  (rust-indent-offset 4))
